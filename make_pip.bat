906294b7854e15b7f32458ed61c966d93139ba5d
Ginklings <andre.ginklings@gmail.com>
2019-07-10 22:16:18 -0300

    Readme edits to PyPi

--- make_pip.bat
+++ make_pip.bat
@@ -1,4 +1,4 @@
-
+rem activate k36
 if exist build\*.* del build\*.* /q /s
 if exist dist\*.* del dist\*.* /q /s
 if exist pysga.egg-info\*.* del pysga.egg-info\*.* /q /s
@@ -8,6 +8,13 @@ rmdir dist
 rmdir pysga.egg-info
 
 python setup.py sdist bdist_wheel
-python -m twine upload dist/*
 
+if "%1"=="v" goto VERIFY
+python -m twine upload dist/*
+goto end
+
+:VERIFY
+python -m twine check dist/*
+
+:end
 pause
--- setup.py
+++ setup.py
@@ -1,27 +1,114 @@
 #!/usr/bin/env python
 
-import os
 from setuptools import setup
 
-__version__ = '1.2.3'
+__version__ = '1.2.7'
 
 
-long_description = ''
-if os.path.exists('README.md'):
-    try:
-        import pypandoc
-        long_description = pypandoc.convert('README.md', 'rst')
-    except(IOError, ImportError):
-        long_description = open('README.md', 'r', encoding='utf8').read()
+long_description = ''' pysga
 
+        =====
+
+        
+
+        A python adaptation for matlab Search Group Algorithm code.
+
+
+        The Search Group Algorithm (SGA) is a metaheuristic optimization method
+        for nonlinear, nonconvex, nonsmooth, multimodal, bounded optimization
+        problems. You may also find a tutorial in a pdf file, which is a step by
+        step explanation about how to use the SGA code. The sections and
+        equations cited in this file refer to the paper that presented the SGA:
+
+        M.S. Gonçalves, R.H. Lopez, L.F.F. Miguel, Search group algorithm: A new
+        metaheuristic method for the optimization of truss structures, Computers
+        & Structures, 153:165-184, 2015. DOI: 0.1016/j.compstruc.2015.03.003
+
+        This paper may also be download at Research Gate:
+
+        https://www.researchgate.net/publication/274253521_Search_group_algorithm_A_new_metaheuristic_method_for_the_optimization_of_truss_structures
+
+        or from science direct at:        
+
+        http://www.sciencedirect.com/science/article/pii/S0045794915000851
+
+        The m-files original codes is provide from:
+
+        https://www.mathworks.com/matlabcentral/fileexchange/50598-search-group-algorithm-matlab-code
+
+        Installation:
+
+        -------------
+
+        Actually is working in python 3.x. The following modules are necessary:
+
+        \* numpy (all)
+        \* kivy (for app only)
+
+        Use pip to install. For only the function without GUI App:
+
+        .. code:: bash
+        
+           pip install pysga
+
+        This will install numpy if necessary.
+
+        For GUI App:
+
+        .. code:: bash
+        
+           pip install pysga[full]
+
+        This will install the kivy module and dependencies. For any error,
+        consult de kivy documentation.
+
+        App example:
+
+        ------------
+
+        .. code:: python
+
+           from pysga.sgaApp import SearchGroupAlgorithmApp
+
+           from kivy.config import Config
+
+           Config.set('graphics', 'width', '500')
+
+           Config.set('graphics', 'height', '600')
+
+           app = SearchGroupAlgorithmApp()
+
+           app.run()
+
+        Put a fobj_function.py file in current directory and define your
+        objective function as fobj function.
+
+        When run the app, choose the from file option and run the optimizer.
+
+        Call SGA in python code:
+
+        ------------------------
+
+        See the github website.
+'''
+# if os.path.exists('README.md'):
+#     try:
+#         import pypandoc
+#         long_description = pypandoc.convert('README.md', 'rst')
+#     except(IOError, ImportError):
+#         long_description = open('README.md', 'r', encoding='utf8').read()
 
 setup(name='pysga',
       version=__version__,
       description='Search Group Algorithm metaheuristic optimization method python adaptation',
+      long_description_content_type='text/markdown',
+      long_description=long_description,
       author='André Ginklings',
       author_email='andre.ginklings@gmail.com',
       url='https://github.com/Ginklings/pysga',
+      license='LICENSE.txt',
       keywords='metaheuristic optimization algorithm',
+      platforms=['Windows', 'Unix'],
       classifiers=[
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
